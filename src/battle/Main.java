a6db97d1e972702f35ee5c7926ef5c3422d6ee34
Bruno Vargas <bruno_jaques@hotmail.com>
2021-04-04 12:02:35 -0300

    Subindo Projeto já criado

---
+++ src/battle/Main.java
@@ -0,0 +1,30 @@
+package battle;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException; 
+
+
+public class Main {
+
+	public static void main(String[] args) throws FailingHttpStatusCodeException, MalformedURLException, IOException, InterruptedException, LineUnavailableException, UnsupportedAudioFileException {	 
+		
+		Play classPlay = new Play();
+
+		while(true) {
+
+			classPlay.logAndPlay();
+		}
+		
+		/**
+		CalculoPrefixoSufixo classe = new CalculoPrefixoSufixo();
+		
+		System.out.println(classe.verificarPalindromo("arara"));//abbabba  BrunoXonurB
+		*/
+	}
+	
+}
